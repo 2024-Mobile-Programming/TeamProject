dfda57d251c3959b793cbf252c9808ee87837c78
zero.wxx <0wkim@naver.com>
2024-11-18 22:11:38 +0900

    author 표시란 추가

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.teamproject;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView author;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        author = (TextView) findViewById(R.id.author);
+
+        // 작가가 공란이면 작가 textView를 메인에 표시하지 않음
+        if (!TextUtils.isEmpty(author.getText())) {
+            author.setVisibility(View.VISIBLE);
+        } else {
+            author.setVisibility(View.GONE);
+        }
     }
 }
